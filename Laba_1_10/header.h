604b13b1a7a16050c2e2bb1e36d88a434254d397
Tim <wartim9494@gmail.com>
2025-02-18 21:15:41 +0300

    half of comments

--- Laba_1_10/header.h
+++ Laba_1_10/header.h
@@ -1,36 +1,46 @@
 #ifndef MY_HEADER
 #define MY_HEADER
 
-// информация о фирме
+//* ==================== максимальные размеры полей структуры firm_info
+#define MAX_NAME_SIZE 62
+#define MAX_TAX_SIZE 9
+#define MAX_DATE_SIZE 22
+
+//*  =================== информация о фирме =================== 
 typedef struct Firm_info 
 {
-    char name[62];
-    char taxes[9];
-    char taxes_deadline[22];
-    char taxes_payed[22];
+    char name[MAX_NAME_SIZE];
+    char taxes[MAX_TAX_SIZE];
+    char taxes_deadline[MAX_DATE_SIZE];
+    char taxes_payed[MAX_DATE_SIZE];
 } firm_info;
 
-firm_info* firm_info_alloc(int number_of_firms);
-void firm_info_free(firm_info* list_of_firms);
+//* ==================== MEMORY =================== 
+firm_info* firm_info_alloc(int number_of_firms);                                    // функция выделения памяти для структуры
+void firm_info_free(firm_info* list_of_firms);                                      // функция освобождения памяти структуры
 
-int is_numeric(const char* str);
-int is_valid_num(const char* str);
-int str_compare(const char* str1, const char* str2);
-int is_valid_date(const char* str);
+//* ==================== CHECKS =================== 
+int is_numeric(const char* str);                                                    // функция проверки на бытиё числом
+int is_valid_num(const char* str);                                                  // функция проверки числа
+int str_compare(const char* str1, const char* str2);                                // функция сравнения строк
+int is_valid_date(const char* str);                                                 // функция проверки даты
 
-int compare(const void* a, const void* b);
-int what_month(const char* str);
-int* find_top_5(firm_info* list_of_firms, int number_of_firms, const char* month);
+//* ==================== TOP-5 =================== 
+int compare(const void* a, const void* b);                                          // функция сравнения
+int what_month(const char* str);                                                    // функция определения порядкогого номера месяца
+int* find_top_5(firm_info* list_of_firms, int number_of_firms, const char* month);  // функция нахождения топ-5 фирм по налогам
 
-void input_int_var(int* a, int t, int min, int max)	;
-const char* input_month();
+//* ==================== INPUT =================== 
+void input_int_var(int* a, int t, int min, int max)	;                               // функция ввода целого числа
+const char* input_month();															// функция ввода месяца
+void input_firm_name(firm_info* list_of_firms, int number_of_firms);				// функция ввода названия фирмы
+void input_firm_taxes(firm_info* list_of_firms, int number_of_firms);				// функция ввода налогов
+void input_firm_dates(firm_info* list_of_firms, int number_of_firms);				// функция ввода дат дедлайна и оплаты налогов
 
-void input_firm_name(firm_info* list_of_firms, int number_of_firms);
-void input_firm_taxes(firm_info* list_of_firms, int number_of_firms);
-void input_firm_dates(firm_info* list_of_firms, int number_of_firms);
+//* ==================== OUTPUT =================== 
+void output_firm_info(firm_info* list_of_firms, int number_of_firms, int t);		// функция вывода данных о фирмах
+void output_top_5(firm_info* list_of_firms, int number_of_firms, const char* month);// функция вывода топ-5 фирм по налогам
 
-void output_firm_info(firm_info* list_of_firms, int number_of_firms, int t);
-void output_top_5(firm_info* list_of_firms, int number_of_firms, const char* month);
-
-void restart_program(int* flag);
+//* ==================== RESTART =================== 
+void restart_program(int* flag);													// функция перезапуска программы
 #endif
