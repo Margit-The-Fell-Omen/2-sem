f628edb28c714ccaebe5de2f12d2456c93e504aa
Tim <wartim9494@gmail.com>
2025-06-03 15:06:56 +0300

    Laba6

---
+++ Laba_6_10/build/CMakeFiles/Laba_6_10.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Laba_6_10.dir/main.c.o"
+  "CMakeFiles/Laba_6_10.dir/main.c.o.d"
+  "Laba_6_10"
+  "Laba_6_10.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Laba_6_10.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Laba_6_10/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>  // Для printf, fgets, sscanf и др. (хотя часть может быть в header.h)
+#include <string.h> // Для strchr (если используется для очистки буфера)
+#include "header.h" // Ваш заголовочный файл
+
+// Имя файла для сохранения и загрузки данных
+#define FILENAME "firms_data.txt"
+
+int main() 
+{
+    int number_of_firms = 0;        // количество фирм
+    int flag = 1;                   // флаг главного цикла программы
+    firm_info* list_of_firms = NULL; // массив структур фирм
+
+    printf("Добро пожаловать в программу учета налогов фирм!\n");
+
+    while (flag) 
+    {
+        // Вызываем функцию menu, передавая адреса list_of_firms и number_of_firms
+        menu(&list_of_firms, &number_of_firms);
+
+        // Вызываем функцию перезапуска/завершения программы
+        restart_program(&flag);
+    }
+
+    // Финальное освобождение памяти, если программа завершается 
+    // и память была выделена в последнем вызове menu
+    if (list_of_firms != NULL) 
+    {
+        firm_info_free(list_of_firms);
+        list_of_firms = NULL;
+    }
+
+    printf("Программа завершена.\n");
+    return 0;
+}
